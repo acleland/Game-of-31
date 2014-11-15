fae394bfe94e0cec1380e3eda111477d6b4ecf96
Andrew Cleland <alcleland@gmail.com>
2014-11-15 09:48:07 -0800

    Initial commit - the code we started in class

---
+++ game31.c
@@ -0,0 +1,19 @@
+/* Andrew and Gina - Game of 31 */
+
+
+typedef struct node_t {
+    char turn;
+    int sum;
+
+    struct node_t* node
+
+
+
+void main(){
+
+    FILE* f1 = fopen("gamedata.txt", "r");
+
+
+
+
+}
